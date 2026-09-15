a8f84b8bad674ce5990fa440ae88bd9422a7b183
Daniel Marques <danmarquesvg@gmail.com>
2026-09-15 11:30:37 -0300

    Aula 04 - While
    Atualização dos arquivos escritos em sala de aula sobre While

--- Coding_Manha_Aulas/src/aula04_Repeticao/MenuCalculadora.java
+++ Coding_Manha_Aulas/src/aula04_Repeticao/MenuCalculadora.java
@@ -8,7 +8,7 @@ public class MenuCalculadora {
 		Scanner scanner = new Scanner(System.in);
 		int opcao = 1;
 		
-		while(opcao >= 1 && opcao <= 4) {
+		while(opcao != 5) {
 			System.out.print("====== CALCULADORA ======\n");
 			System.out.println();
 			System.out.print("Opções\n");
@@ -21,19 +21,48 @@ public class MenuCalculadora {
 			System.out.print("Digite um valor: ");
 			opcao = scanner.nextInt();
 			
-			System.out.println("Digite o primeiro valor: ");
-			int primeiroValor = scanner.nextInt();
-			System.out.println("Digite o segundo valor: ");
-			int segundoValor = scanner.nextInt();
-			
 			if(opcao == 1) {
+				System.out.println("Digite o primeiro valor: ");
+				int primeiroValor = scanner.nextInt();
+				System.out.println("Digite o segundo valor: ");
+				int segundoValor = scanner.nextInt();
+				
 				int soma = primeiroValor + segundoValor;
 				System.out.println("Soma = "+soma);
+				
 			} else if(opcao == 2) {
+				System.out.println("Digite o primeiro valor: ");
+				int primeiroValor = scanner.nextInt();
+				System.out.println("Digite o segundo valor: ");
+				int segundoValor = scanner.nextInt();
+				
 				int subtracao = primeiroValor - segundoValor;
 				System.out.println("Subtração = "+subtracao);
+			} else if(opcao == 3) {
+				System.out.println("Digite o primeiro valor: ");
+				int primeiroValor = scanner.nextInt();
+				System.out.println("Digite o segundo valor: ");
+				int segundoValor = scanner.nextInt();
+				
+				int multiplicacao = primeiroValor * segundoValor;
+				System.out.println("Multiplicação = "+multiplicacao);
+			} else if (opcao == 4) {
+				System.out.println("Digite o primeiro valor: ");
+				double primeiroValor = scanner.nextDouble();
+				System.out.println("Digite o segundo valor: ");
+				double segundoValor = scanner.nextDouble();
+				
+				if(segundoValor == 0) {
+					System.out.println("Não é possível dividir por zero");
+				} else {
+					double divisao = (double)(primeiroValor / segundoValor);
+					System.out.println("Divisão = "+divisao);
+				}
+			} else if(opcao == 5){
+				System.out.println("Encerrando o programa");
+			} else {
+				System.out.println();
 			}
-			
 		}
 	}
 
